e28d9cfb1ec9e6c00be7ac774fa4d6f917630291
Tuan Nguyen <tuan.nguyen@hiivesystems.com>
2019-02-19 22:27:58 +1100

    Release v1.1.7

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 
 to="$1"
 force="$2"
-find . -name 'pom.xml' -exec perl -i~ -0777 -pe "s@(<groupId>com\.tuannguyen\.liquibase</groupId>\s+)<version>(.*?)</version>@\1<version>$to</version>@g" {} \;
+find . -name 'pom.xml' -exec perl -i -0777 -pe "s@(<groupId>com\.tuannguyen\.liquibase</groupId>\s+)<version>(.*?)</version>@\1<version>$to</version>@g" {} \;
 git commit -a -m "Release v$to";
 git tag -d "v$to"
 git push origin -d "v$to"
